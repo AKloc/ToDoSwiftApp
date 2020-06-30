48426e7beaf9179bd744e77e5d1d55a448e5ac94
akloc@github.com <akloc@github.com>
2020-06-30 19:45:19 -0400

    First commit, out of the box Xcode 12 Beta project.

---
+++ ToDo Swift App/Shared/ToDo_Swift_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_Swift_AppApp.swift
+//  Shared
+//
+//  Created by Andy Kloc on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_Swift_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
